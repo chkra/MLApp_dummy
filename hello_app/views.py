1076e9226e451cb235a0df6d9be8c09b6e83cfc2
Christina Kratsch <christina.kratsch@htw-berlin.de>
2023-01-17 15:18:51 +0100

    Added functionality for an actual ML prediction

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,6 +1,12 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from . import app
+
+import numpy as np
+import pickle
+
+model = pickle.load(open('iris_model.pkl', 'rb')) # loading the trained model
+
 
 @app.route("/")
 def home():
@@ -26,3 +32,19 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+
+@app.route("/iris")
+def iris():
+    return render_template("predict.html")
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    # retrieving values from form
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+    prediction = model.predict(final_features) # making prediction
+    return render_template('predict.html', prediction_text='Predicted Class: {}'.format(prediction)) # rendering the predicted result
